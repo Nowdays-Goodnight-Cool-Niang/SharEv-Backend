2f5e50957678c9cae119c4535166b27859e39ecf
eora21 <eora21@naver.com>
2025-01-12 02:20:44 +0900

    refactor: 💡 참여 데이터가 없는 경우의 생성 로직 더 효율적으로 변경

--- src/main/java/org/example/backend/participant/service/ParticipantService.java
+++ src/main/java/org/example/backend/participant/service/ParticipantService.java
@@ -12,6 +12,8 @@ import org.example.backend.participant.repository.ParticipantRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -24,17 +26,28 @@ public class ParticipantService {
     public void putParticipant(Account account, RequestPutParticipantInfo requestPutParticipantInfo) {
         Long eventId = requestPutParticipantInfo.eventId();
 
-        Participant participant = participantRepository.findByEventIdAndAccountId(eventId, account.getId())
-                .orElseGet(() -> createParticipant(eventId, account));
+        Optional<Participant> optionalParticipant = participantRepository.findByEventIdAndAccountId(eventId, account.getId());
+
+        if (optionalParticipant.isEmpty()) {
+            createParticipant(eventId, account, requestPutParticipantInfo);
+            return;
+        }
+
+        Participant participant = optionalParticipant.get();
 
         participant.setJobGroup(requestPutParticipantInfo.jobGroup());
         participant.setTeamName(requestPutParticipantInfo.teamName());
         participant.setProjectInfo(requestPutParticipantInfo.projectInfo());
     }
 
-    private Participant createParticipant(Long eventId, Account account) {
-        Event event = eventRepository.findById(eventId).orElseThrow();
-        return participantRepository.save(new Participant(event, account));
+    private void createParticipant(Long eventId, Account account, RequestPutParticipantInfo requestPutParticipantInfo) {
+        Event event = eventRepository.getReferenceById(eventId);
+
+        String jobGroup = requestPutParticipantInfo.jobGroup();
+        String teamName = requestPutParticipantInfo.teamName();
+        String projectInfo = requestPutParticipantInfo.projectInfo();
+
+        participantRepository.save(new Participant(event, account, jobGroup, teamName, projectInfo));
     }
 
     public ResponseGetParticipantInfo fetchParticipants(Long accountId, Long participantId) {
