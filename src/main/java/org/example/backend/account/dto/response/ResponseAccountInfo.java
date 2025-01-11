648435b8f89aa629a4cdb754c022b105bffb3b62
jjeonghak <rlawjdgks3185@khu.ac.kr>
2025-01-12 05:21:22 +0900

    refactor: 💡 사용자 상세 정보 dto 수정

--- src/main/java/org/example/backend/account/dto/response/ResponseAccountInfo.java
+++ src/main/java/org/example/backend/account/dto/response/ResponseAccountInfo.java
@@ -9,11 +9,17 @@ public class ResponseAccountInfo {
     private final String name;
     private final String phone;
     private final int profileImageId;
+    private final String github;
+    private final String instagram;
+    private final String facebook;
 
     public ResponseAccountInfo(Account account) {
         this.id = account.getId();
         this.name = account.getName();
         this.phone = account.getPhone();
         this.profileImageId = account.getProfileImageId();
+        this.github = account.getGithubUrl();
+        this.instagram = account.getInstagramUrl();
+        this.facebook = account.getFacebookUrl();
     }
 }
