0c0bce27a575e7eb141ac6184b94be11506d0fb7
eora21 <eora21@naver.com>
2025-01-11 20:37:50 +0900

    feat: 🎸 사용자 정보 업데이트 api 작성

---
+++ src/main/java/org/example/backend/account/service/AccountService.java
@@ -0,0 +1,19 @@
+package org.example.backend.account.service;
+
+import org.example.backend.account.dto.request.RequestUpdateInfoDto;
+import org.example.backend.account.entity.Account;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class AccountService {
+    public void updateAccountInfo(Account account, RequestUpdateInfoDto requestUpdateInfoDto) {
+        account.setName(requestUpdateInfoDto.name());
+        account.setPhone(requestUpdateInfoDto.phone());
+        account.setProfileImageId(requestUpdateInfoDto.profileImageId());
+        account.setGithubUrl(requestUpdateInfoDto.githubUrl());
+        account.setInstagramUrl(requestUpdateInfoDto.instagramUrl());
+        account.setFacebook(requestUpdateInfoDto.facebookUrl());
+    }
+}
