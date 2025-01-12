36f9f805e812f60052611691bd2fa39ac95f9375
eora21 <eora21@naver.com>
2025-01-12 10:18:40 +0900

    fix: 🐛 엔티티 매니저 영역까지 전달되도록 정정

--- src/main/java/org/example/backend/account/service/AccountService.java
+++ src/main/java/org/example/backend/account/service/AccountService.java
@@ -1,20 +1,26 @@
 package org.example.backend.account.service;
 
+import lombok.RequiredArgsConstructor;
 import org.example.backend.account.dto.request.RequestUpdateInfoDto;
 import org.example.backend.account.entity.Account;
+import org.example.backend.account.repository.AccountRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class AccountService {
+    private final AccountRepository accountRepository;
+
     @Transactional
     public void updateAccountInfo(Account account, RequestUpdateInfoDto requestUpdateInfoDto) {
-        account.setName(requestUpdateInfoDto.name());
-        account.setPhone(requestUpdateInfoDto.phone());
-        account.setProfileImageId(requestUpdateInfoDto.profileImageId());
-        account.setGithubUrl(requestUpdateInfoDto.githubUrl());
-        account.setInstagramUrl(requestUpdateInfoDto.instagramUrl());
-        account.setFacebookUrl(requestUpdateInfoDto.facebookUrl());
+        Account accountEntity = accountRepository.findById(account.getId()).orElseThrow();
+        accountEntity.setName(requestUpdateInfoDto.name());
+        accountEntity.setPhone(requestUpdateInfoDto.phone());
+        accountEntity.setProfileImageId(requestUpdateInfoDto.profileImageId());
+        accountEntity.setGithubUrl(requestUpdateInfoDto.githubUrl());
+        accountEntity.setInstagramUrl(requestUpdateInfoDto.instagramUrl());
+        accountEntity.setFacebookUrl(requestUpdateInfoDto.facebookUrl());
     }
 }
