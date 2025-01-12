3c9437cf693076a5acee9e5506d05295ececf4ee
jjeonghak <rlawjdgks3185@khu.ac.kr>
2025-01-12 09:08:07 +0900

    test: 💍 데이터베이스 더미 데이터 추가

--- mysql/ddl.sql
+++ mysql/ddl.sql
@@ -61,6 +61,27 @@ VALUES (1, '삐약톤: 캠퍼스 대항전',
         '2025-01-11 11:00:00', '2025-01-12 14:00:00', '동국대학교 서울캠퍼스 혜화관 고순청 세미나실',
         'GDG Campus Korea', 'https://cf.festa.io/img/2024-11-22/cd01d222-e60e-455e-b961-734e30658f2f.png', 'https://festa.io/events/6381'),
        (2, '오픈소스 컨트리뷰톤', '오픈소스 프로젝트 기여 행사', '2025-05-20 09:00:00', '2025-05-20 18:00:00', '강남 개발자 센터', '오픈소스 커뮤니티',
-        'https://oopy.lazyrockets.com/api/v2/notion/image?src=https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Ff8882592-3f24-4f4d-8f7a-51e03c5c45ff%2F07e66aa8-aed1-4a3a-acac-faf1b3d6ce32%2F2024_OSSCA_ossportal_title_image_broken.jpg&blockId=5b6265b1-e29e-4d7a-b5d4-3c58fe07cda8', 'https://www.contribution.ac/ossca'),
-       (3, '2024 개발자 컨퍼런스', '최신 기술 트렌드와 개발 경험을 공유하는 컨퍼런스', '2024-03-15 10:00:00', '2024-03-15 18:00:00', '코엑스 그랜드볼룸',
-        '테크 커뮤니티', 'https://example.com/images/conf2024.jpg', 'https://example.com/events/dev-conf-2024');
+        'https://www.oss.kr/plugins/oss/components/Modules/Contributhon/assets/img/2024 OSSCA-ossportal-title image_v1.0.jpg', 'https://www.contribution.ac/ossca'),
+       (3, '모든 연결을 새롭게, if(kakaoAI)2024', '최신 기술 트렌드와 개발 경험을 공유하는 컨퍼런스', '2024-03-15 10:00:00', '2024-03-15 18:00:00', '코엑스 그랜드볼룸',
+        '카카오 테크', 'https://t1.kakaocdn.net/kakao_tech/image/2d7767bd019200001.png', 'https://tech.kakao.com/posts/636');
+
+INSERT INTO `accounts`
+(`account_id`, `name`, `phone`, `github_url`, `instagram_url`, `facebook_url`, `profile_image_id`)
+VALUES (10, 'name01', '01012345678', 'www.github.com/jjeonghak', null, null, 1),
+    (11, 'name02', '01012345679', null, null, null, 3),
+    (12, null, null, null, null, null, 1)
+
+INSERT INTO `participants`
+(`participant_id`, `event_id`, `account_id`, `job_group`, `team_name`, `project_info`)
+VALUES (1, 1, 10, 'jobGroup01', 'teamName01', 'projectInfo01'),
+       (2, 1, 11, 'jobGroup02', 'teamName02', 'projectInfo02');
+       (3, 1, 12, 'jobGroup03', 'teamName03', 'projectInfo03');
+       (4, 1, 1, 'jobGroup04', 'teamName04', 'projectInfo04');
+
+INSERT INTO `found_participants`
+(`participant_id`, `account_id`)
+VALUES (1, 11),
+    (1, 12),
+    (4, 12),
+    (4, 11);
+
