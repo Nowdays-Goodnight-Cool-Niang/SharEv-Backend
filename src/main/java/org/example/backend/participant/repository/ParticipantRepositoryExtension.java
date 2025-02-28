15067ceedaed62e6b841aba6ac33279f819eb422
eora21 <eora21@naver.com>
2025-02-28 23:53:42 +0900

    refactor: 💡 MVP를 위한 코드 삭제
    최소한의 api만 운영하기 위해 기존 코드 제거

--- src/main/java/org/example/backend/participant/repository/ParticipantRepositoryExtension.java
+++
@@ -1,9 +0,0 @@
-package org.example.backend.participant.repository;
-
-import org.example.backend.participant.dto.response.ResponseGetParticipantInfo;
-
-public interface ParticipantRepositoryExtension {
-
-    ResponseGetParticipantInfo fetchDetailsBy(Long id);
-
-}
