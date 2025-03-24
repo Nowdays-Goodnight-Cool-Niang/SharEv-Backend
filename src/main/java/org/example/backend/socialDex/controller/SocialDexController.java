d04995d1cf99ae5caa59ed528bbc61faaa386fe6
eora21 <eora21@naver.com>
2025-03-24 22:31:30 +0900

    refactor: 💡 api 엔드포인트만 participants로 변경

--- src/main/java/org/example/backend/socialDex/controller/SocialDexController.java
+++ src/main/java/org/example/backend/socialDex/controller/SocialDexController.java
@@ -12,7 +12,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/social-dex")
+@RequestMapping("/participants")
 @RequiredArgsConstructor
 public class SocialDexController {
 
