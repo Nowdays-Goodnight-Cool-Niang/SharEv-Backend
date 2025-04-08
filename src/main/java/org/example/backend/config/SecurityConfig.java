c611eb8ac5c024cd6cb7f0a2dc1a4d0b4dc19f12
eora21 <eora21@naver.com>
2025-04-08 20:31:36 +0900

    fix: 🐛 docker 프로필 환경에서만 5173 포트 허용

--- src/main/java/org/example/backend/config/SecurityConfig.java
+++ src/main/java/org/example/backend/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.backend.account.entity.Account;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -71,6 +72,7 @@ public class SecurityConfig {
     }
 
     @Bean
+    @Profile("docker")
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
