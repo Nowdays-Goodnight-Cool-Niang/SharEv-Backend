5c03c242b2f68f856f65361b72ce56598d2ff96d
eora21 <eora21@naver.com>
2025-04-09 10:34:03 +0900

    test: 💍 조회 시작 시간 이후로 도감에 추가된 사용자 정보 반영 테스트 작성

--- src/test/java/org/example/backend/socialDex/repository/SocialDexRepositoryTest.java
+++ src/test/java/org/example/backend/socialDex/repository/SocialDexRepositoryTest.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -62,7 +63,7 @@ class SocialDexRepositoryTest {
         em.persist(new SocialDex(secondAccount, fourthAccount));
 
         // When
-        Page<ResponseSocialDexInfoDto.AccountInfo> result = socialDexRepository.findDexParticipants(thirdAccount.getId(), PageRequest.of(0, 2));
+        Page<ResponseSocialDexInfoDto.AccountInfo> result = socialDexRepository.findDexParticipants(thirdAccount.getId(), LocalDateTime.now(), PageRequest.of(0, 2));
 
         // Then
         assertNotNull(result);
@@ -70,5 +71,56 @@ class SocialDexRepositoryTest {
         assertEquals(2, result.getTotalPages());
         assertEquals(2, result.get().count());
         assertEquals(List.of("five@email.com", "four@email.com"), result.get().map(ResponseSocialDexInfoDto.AccountInfo::email).toList());
+    }
+
+    @Test
+    @DisplayName("실시간 도감 반영")
+    void afterRegistered() {
+
+        // Given
+        Account firstAccount = new Account(1L, "1번 유저");
+        firstAccount.setEmail("one@email.com");
+
+        Account secondAccount = new Account(2L, "2번 유저");
+        secondAccount.setEmail("two@email.com");
+
+        Account thirdAccount = new Account(3L, "3번 유저");
+        thirdAccount.setEmail("three@email.com");
+
+        Account fourthAccount = new Account(4L, "4번 유저");
+        fourthAccount.setEmail("four@email.com");
+
+        Account fifthAccount = new Account(5L, "5번 유저");
+        fifthAccount.setEmail("five@email.com");
+
+        em.persist(firstAccount);
+        em.persist(secondAccount);
+        em.persist(thirdAccount);
+        em.persist(fourthAccount);
+        em.persist(fifthAccount);
+
+        // When
+        em.persist(new SocialDex(thirdAccount, fifthAccount));
+        LocalDateTime snapshotTime = LocalDateTime.now();
+        Page<ResponseSocialDexInfoDto.AccountInfo> result = socialDexRepository.findDexParticipants(thirdAccount.getId(), snapshotTime, PageRequest.of(0, 2));
+
+        // Then
+        assertNotNull(result);
+        assertEquals(4, result.getTotalElements());
+        assertEquals(2, result.getTotalPages());
+        assertEquals(2, result.get().count());
+        assertEquals(List.of("five@email.com", "one@email.com"), result.get().map(ResponseSocialDexInfoDto.AccountInfo::email).toList());
+
+        // When
+        em.persist(new SocialDex(thirdAccount, fourthAccount));
+        result = socialDexRepository.findDexParticipants(thirdAccount.getId(), snapshotTime, PageRequest.of(1, 2));
+
+        // Then
+        assertNotNull(result);
+        assertEquals(4, result.getTotalElements());
+        assertEquals(2, result.getTotalPages());
+        assertEquals(2, result.get().count());
+        assertEquals(List.of("two@email.com", "four@email.com"), result.get().map(ResponseSocialDexInfoDto.AccountInfo::email).toList());
+        assertEquals(List.of(false, true), result.get().map(ResponseSocialDexInfoDto.AccountInfo::registerFlag).toList());
     }
 }
