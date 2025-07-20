dd7fc5a2626f864a63d4395e78a187376fe66c15
eora21 <eora21@naver.com>
2025-07-20 12:57:10 +0900

    fix: 🐛 lock prefix를 추가하여 기존 redis 데이터 내 겹치는 키가 없도록 수정

--- src/main/java/org/example/backend/util/LockProcessor.java
+++ src/main/java/org/example/backend/util/LockProcessor.java
@@ -12,13 +12,14 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class LockProcessor {
+    private static final String LOCK_PREFIX = "redisson-lock";
     private static final int WAIT_TIME = 60;
     private static final int LEASE_TIME = 5;
 
     private final RedissonClient redissonClient;
 
     public void lock(String key, Consumer<String> consumer) {
-        RLock lock = redissonClient.getLock(key);
+        RLock lock = redissonClient.getLock(String.join("-", LOCK_PREFIX, key));
         boolean lockSuccessFlag = false;
 
         try {
