07522fe2e8bbf5aa7142bd3aa4ee8a71e74f220a
eora21 <eora21@naver.com>
2025-07-20 23:38:27 +0900

    refactor: 💡 Event entity NoArgsConstructor public으로 변경
    jpa를 위해 protected를 거는게 기본, 하지만 현재 해당 entity를 위한 생성자가 따로 없으므로 일단 public
    설정

--- src/main/java/org/example/backend/event/entity/Event.java
+++ src/main/java/org/example/backend/event/entity/Event.java
@@ -7,14 +7,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.util.UUID;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.backend.base_entity.BaseTimeEntity;
 
 @Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 @Table(name = "events")
 public class Event extends BaseTimeEntity {
     @Id
