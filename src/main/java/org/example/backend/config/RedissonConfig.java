f253a4fc794c29f28bb203c95bb53f6fc44f220e
eora21 <eora21@naver.com>
2025-07-25 19:04:11 +0900

    refactor: 💡 redisson 비밀번호 관련 로직 강화

--- src/main/java/org/example/backend/config/RedissonConfig.java
+++ src/main/java/org/example/backend/config/RedissonConfig.java
@@ -1,9 +1,11 @@
 package org.example.backend.config;
 
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.redisson.config.SingleServerConfig;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,9 +20,13 @@ public class RedissonConfig {
         Config config = new Config();
         String host = redisProperties.getHost();
         int port = redisProperties.getPort();
-        config.useSingleServer()
-                .setAddress(REDISSON_HOST_PREFIX + host + ":" + port)
-                .setPassword(redisProperties.getPassword());
+        SingleServerConfig serverConfig = config.useSingleServer();
+        serverConfig.setAddress(REDISSON_HOST_PREFIX + host + ":" + port);
+
+        String password = redisProperties.getPassword();
+        if (Objects.nonNull(password) && !password.isEmpty()) {
+            serverConfig.setPassword(password);
+        }
 
         return Redisson.create(config);
     }
