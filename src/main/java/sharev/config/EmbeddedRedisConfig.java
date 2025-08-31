412e142c83c4e50a960d9d457ed1081574cf34fc
eora21 <eora21@naver.com>
2025-08-31 10:02:25 +0900

    fix: 🐛 redisServer 생성 타이밍 문제로 인한 프로젝트 실패 문제 해결

--- src/main/java/sharev/config/EmbeddedRedisConfig.java
+++ src/main/java/sharev/config/EmbeddedRedisConfig.java
@@ -1,40 +1,31 @@
 package sharev.config;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
 import java.io.IOException;
 import java.net.ServerSocket;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import redis.embedded.RedisServer;
 
 @Profile("!prod")
 @Configuration
 public class EmbeddedRedisConfig {
-    private static final String REDIS_SERVER_MAX_MEMORY = "maxmemory 512M";
-    private RedisServer redisServer;
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public RedisServer redisServer() throws IOException {
+        return new RedisServer(findAvailablePort());
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(RedisServer redisServer) {
+        return new LettuceConnectionFactory("localhost", redisServer.ports().get(0));
+    }
 
     private int findAvailablePort() throws IOException {
         try (ServerSocket socket = new ServerSocket(0)) {
             return socket.getLocalPort();
-        }
-    }
-
-    @PostConstruct
-    public void startRedis() throws IOException {
-        int port = findAvailablePort();
-        redisServer = RedisServer.newRedisServer()
-                .port(port)
-                .setting(REDIS_SERVER_MAX_MEMORY)
-                .build();
-        redisServer.start();
-        System.setProperty("spring.data.redis.port", String.valueOf(port));
-    }
-
-    @PreDestroy
-    public void stopRedis() throws IOException {
-        if (redisServer != null) {
-            redisServer.stop();
         }
     }
 }
--- src/main/java/sharev/config/RedissonConfig.java
+++ src/main/java/sharev/config/RedissonConfig.java
@@ -1,6 +1,7 @@
 package sharev.config;
 
 import java.util.Objects;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
@@ -9,18 +10,32 @@ import org.redisson.config.SingleServerConfig;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import redis.embedded.RedisServer;
 
 @Configuration
 @RequiredArgsConstructor
 public class RedissonConfig {
     private static final String REDISSON_HOST_PREFIX = "redis://";
 
+    private final RedisProperties redisProperties;
+    private final Optional<RedisServer> embeddedRedisServer;
+
     @Bean
-    public RedissonClient redissonClient(RedisProperties redisProperties) {
+    public RedissonClient redissonClient() {
         Config config = new Config();
-        String host = redisProperties.getHost();
-        int port = redisProperties.getPort();
         SingleServerConfig serverConfig = config.useSingleServer();
+
+        final String host;
+        final int port;
+
+        if (embeddedRedisServer.isPresent()) {
+            host = "localhost";
+            port = embeddedRedisServer.get().ports().get(0);
+        } else {
+            host = redisProperties.getHost();
+            port = redisProperties.getPort();
+        }
+
         serverConfig.setAddress(REDISSON_HOST_PREFIX + host + ":" + port);
 
         String password = redisProperties.getPassword();
