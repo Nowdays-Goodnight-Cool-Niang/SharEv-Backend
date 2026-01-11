14bb23d3deddf07631166aeb70efd3e5c0c97ee8
eora21 <eora21@naver.com>
2026-01-11 16:53:29 +0900

    fix: 🐛 swagger 경로 허용하도록 구성

--- src/main/java/sharev/config/SecurityConfig.java
+++ src/main/java/sharev/config/SecurityConfig.java
@@ -59,6 +59,8 @@ public class SecurityConfig {
         http.authorizeHttpRequests(authorizeRequests -> {
             authorizeRequests.requestMatchers("/actuator/**")
                     .permitAll();
+            authorizeRequests.requestMatchers("/swagger-ui/**")
+                    .permitAll();
             authorizeRequests.requestMatchers(HttpMethod.POST, "/signup", "/login")
                     .permitAll();
             authorizeRequests.anyRequest()
