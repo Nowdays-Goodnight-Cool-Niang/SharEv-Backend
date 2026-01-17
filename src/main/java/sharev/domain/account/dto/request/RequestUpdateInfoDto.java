05341ad75ee488da95b2b2cef6392ff3d405c1f7
eora21 <eora21@naver.com>
2026-01-17 18:05:25 +0900

    fix: 🐛 remove unused fields

--- src/main/java/sharev/domain/account/dto/request/RequestUpdateInfoDto.java
+++ src/main/java/sharev/domain/account/dto/request/RequestUpdateInfoDto.java
@@ -3,7 +3,6 @@ package sharev.domain.account.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import org.hibernate.validator.constraints.URL;
 
 public record RequestUpdateInfoDto(
         @NotBlank
@@ -12,18 +11,6 @@ public record RequestUpdateInfoDto(
 
         @Email
         @Size(max = 320)
-        String email,
-
-        @URL
-        @Size(max = 255)
-        String linkedinUrl,
-
-        @URL
-        @Size(max = 255)
-        String githubUrl,
-
-        @URL
-        @Size(max = 255)
-        String instagramUrl
+        String email
 ) {
 }
