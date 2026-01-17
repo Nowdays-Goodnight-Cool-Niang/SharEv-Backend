104a0d3aea532b0b12bb1ea361992196a8661d7e
eora21 <eora21@naver.com>
2026-01-17 18:03:30 +0900

    fix: 🐛 find my teams querydsl

--- src/main/java/sharev/domain/team/repository/TeamRepositoryImpl.java
+++ src/main/java/sharev/domain/team/repository/TeamRepositoryImpl.java
@@ -1,11 +1,13 @@
 package sharev.domain.team.repository;
 
-import static sharev.domain.account.entity.QAccount.account;
 import static sharev.domain.member.entity.QMember.member;
+import static sharev.domain.team.entity.QTeam.team;
 
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import sharev.domain.member.entity.QMember;
 import sharev.domain.team.dto.response.QResponseTeamInfoDto;
 import sharev.domain.team.dto.response.ResponseTeamInfoDto;
 
@@ -14,20 +16,25 @@ public class TeamRepositoryImpl implements TeamRepositoryCustom {
     private final JPAQueryFactory queryFactory;
 
     public List<ResponseTeamInfoDto> findMyTeams(Long accountId) {
+        QMember teamMember = new QMember("teamMember");
+
         return queryFactory
                 .select(new QResponseTeamInfoDto(
-                        member.team.id,
-                        member.team.title,
-                        member.team.content,
-                        member.team.createdAt,
-                        member.count().intValue()
+                        team.id,
+                        team.title,
+                        team.content,
+                        team.createdAt,
+                        JPAExpressions
+                                .select(teamMember.count().intValue())
+                                .from(teamMember)
+                                .where(teamMember.team.eq(team))
                 ))
-                .from(account)
-                .where(account.id.eq(accountId))
+                .from(member)
 
-                .leftJoin(member)
-                .on(account.eq(member.account))
+                .join(team)
+                .on(member.team.eq(team))
 
+                .where(member.account.id.eq(accountId))
                 .fetch();
     }
 }
