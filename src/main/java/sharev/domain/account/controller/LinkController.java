df71c2cb8132dc6dbbd1506690e38f4f57828a97
eora21 <eora21@naver.com>
2026-02-01 11:33:14 +0900

    fix: 🐛 누락된 RequestBody 어노테이션 추가

--- src/main/java/sharev/domain/account/controller/LinkController.java
+++ src/main/java/sharev/domain/account/controller/LinkController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sharev.domain.account.dto.request.RequestCreateLinkDto;
@@ -25,7 +26,7 @@ public class LinkController {
 
     @PostMapping
     public ResponseEntity<ResponseCreateLinkDto> addLink(@AuthenticationPrincipal Account account,
-                                                         RequestCreateLinkDto requestCreateLinkDto) {
+                                                         @RequestBody RequestCreateLinkDto requestCreateLinkDto) {
 
         ResponseCreateLinkDto responseCreateLinkDto = linkService.addLink(account, requestCreateLinkDto.url());
 
@@ -40,7 +41,7 @@ public class LinkController {
 
     @DeleteMapping
     public ResponseEntity<Void> deleteLink(@AuthenticationPrincipal Account account,
-                                           RequestDeleteLinkDto requestDeleteLinkDto) {
+                                           @RequestBody RequestDeleteLinkDto requestDeleteLinkDto) {
 
         linkService.removeLink(account, requestDeleteLinkDto.linkId());
 
