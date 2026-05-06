9de3474b23c90484a48274a511e3b45268443b2a
eora21 <eora21@naver.com>
2026-05-06 16:00:51 +0900

    feat: 🎸 피드백 이벤트 리스너 작성

---
+++ src/main/java/sharev/feedback/adapter/inbound/event/FeedbackEventListener.kt
@@ -0,0 +1,21 @@
+package sharev.feedback.adapter.inbound.event
+
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Component
+import org.springframework.transaction.event.TransactionPhase
+import org.springframework.transaction.event.TransactionalEventListener
+import sharev.account.domain.event.AccountWithdrawalFeedbackSubmittedEvent
+import sharev.feedback.application.port.inbound.command.SaveFeedbackCommand
+import sharev.feedback.application.port.inbound.usecase.SaveFeedbackUseCase
+
+@Component
+class FeedbackEventListener(
+    private val saveFeedbackUseCase: SaveFeedbackUseCase,
+) {
+
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    fun listen(event: AccountWithdrawalFeedbackSubmittedEvent) {
+        saveFeedbackUseCase.save(SaveFeedbackCommand(event.feedback))
+    }
+}
