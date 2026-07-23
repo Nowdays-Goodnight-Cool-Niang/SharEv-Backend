5b17ca42ea0563fdafd1b79a45d69c958efabc58
eora21 <eora21@naver.com>
2026-07-23 16:14:16 +0900

    refactor: 💡 null 허용되지 않는 파라미터 직렬화 시 발생 문제 핸들러로 관리 + 클래스명 변경

--- src/main/kotlin/sharev/common/adapter/inbound/web/advice/InvalidRequestExceptionAdvice.kt
+++ src/main/kotlin/sharev/common/adapter/inbound/web/advice/InvalidRequestExceptionAdvice.kt
@@ -1,7 +1,9 @@
 package sharev.common.adapter.inbound.web.advice
 
+import com.fasterxml.jackson.databind.exc.MismatchedInputException
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.util.LinkedMultiValueMap
 import org.springframework.util.MultiValueMap
 import org.springframework.validation.BindException
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
-class BindExceptionAdvice {
+class InvalidRequestExceptionAdvice {
 
     @ExceptionHandler(BindException::class)
     fun handleBindException(exception: BindException): ResponseEntity<MultiValueMap<String, String>> {
@@ -32,6 +34,23 @@ class BindExceptionAdvice {
 
         return fieldError.defaultMessage
             ?: "조건이 충족되지 않았습니다."
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun handleNotReadable(exception: HttpMessageNotReadableException): ResponseEntity<MultiValueMap<String, String>> {
+        val errorMessages = LinkedMultiValueMap<String, String>()
+        val cause = exception.cause
+
+        if (cause is MismatchedInputException && cause.path.isNotEmpty()) {
+            val field = cause.path.lastOrNull()?.fieldName ?: "request"
+            errorMessages.add(field, "필수 값이거나 형식이 올바르지 않습니다.")
+        } else {
+            errorMessages.add("request", "요청 본문을 해석할 수 없습니다.")
+        }
+
+        return ResponseEntity.badRequest()
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(errorMessages)
     }
 }
 
