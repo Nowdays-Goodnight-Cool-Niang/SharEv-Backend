cdae56cdc7a2b892ead7da93b36320b4c01b402a
eora21 <eora21@naver.com>
2026-07-30 16:28:21 +0900

    refactor: 💡 검증 로직 서비스로 이동, handle 업데이트 로직 구성

--- src/main/kotlin/sharev/account/application/port/inbound/mapper/AccountInboundMapper.kt
+++ src/main/kotlin/sharev/account/application/port/inbound/mapper/AccountInboundMapper.kt
@@ -1,6 +1,11 @@
 package sharev.account.application.port.inbound.mapper
 
+import sharev.account.application.port.inbound.result.UpdateAccountHandleResult
 import sharev.account.application.port.inbound.result.UpdateAccountInfoResult
 import sharev.account.domain.model.Account
 
 fun Account.toUpdateAccountInfoResult() = UpdateAccountInfoResult(id, name, email)
+
+fun Account.toUpdateAccountHandleResult() = UpdateAccountHandleResult(
+    checkNotNull(handle) { "handle 업데이트 직후에는 null이어선 안 됩니다." }
+)
