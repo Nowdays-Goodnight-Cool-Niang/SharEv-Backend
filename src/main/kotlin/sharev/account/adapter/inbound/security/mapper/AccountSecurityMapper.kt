77a00e097632a8be98e024fba067e973011e9743
eora21 <eora21@naver.com>
2026-07-30 16:30:12 +0900

    refactor: 💡 OAuthLoginResult -> principal 전환 중 누락된 handle 추가

--- src/main/kotlin/sharev/account/adapter/inbound/security/mapper/AccountSecurityMapper.kt
+++ src/main/kotlin/sharev/account/adapter/inbound/security/mapper/AccountSecurityMapper.kt
@@ -4,4 +4,4 @@ import sharev.account.application.port.inbound.result.OAuthLoginResult
 import sharev.common.adapter.inbound.security.model.AccountPrincipal
 
 fun OAuthLoginResult.toPrincipal(attributes: Map<String, Any>) =
-    AccountPrincipal(id, role.name, name, email, attributes)
+    AccountPrincipal(id, role.name, name, email, handle, attributes)
