a2b89d3cd576554243c8c330d64fd5d92ffafcc6
eora21 <eora21@naver.com>
2026-08-04 13:30:06 +0900

    feat: 🎸 팀 정보 수정 책임을 도메인으로 이동, 필드 순서 및 content nullable로 수정

--- src/main/kotlin/sharev/team/domain/model/Team.kt
+++ src/main/kotlin/sharev/team/domain/model/Team.kt
@@ -1,13 +1,22 @@
 package sharev.team.domain.model
 
+import sharev.team.domain.exception.TeamException
+import sharev.team.domain.exception.TeamExceptionCode
 import java.time.LocalDateTime
 
 data class Team(
     val id: Long,
-    val teamCertification: TeamCertification,
-    val teamType: TeamType,
     val title: String?,
-    val content: String,
+    val content: String?,
+    val certification: TeamCertification,
+    val type: TeamType,
     val createdAt: LocalDateTime?,
 ) {
+    fun updateInfo(title: String, content: String): Team {
+        if (type != TeamType.PUBLIC) {
+            throw TeamException(TeamExceptionCode.NOT_MODIFIABLE_TEAM)
+        }
+
+        return copy(title = title, content = content)
+    }
 }
