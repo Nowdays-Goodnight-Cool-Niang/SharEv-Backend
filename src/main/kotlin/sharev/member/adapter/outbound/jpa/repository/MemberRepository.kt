0efbc6df68359218840b044a3a725f451cb67d9e
eora21 <eora21@naver.com>
2026-08-05 10:51:30 +0900

    refactor: 💡 사용되지 않던 memberRepository 메서드 제거

--- src/main/kotlin/sharev/member/adapter/outbound/jpa/repository/MemberRepository.kt
+++ src/main/kotlin/sharev/member/adapter/outbound/jpa/repository/MemberRepository.kt
@@ -2,15 +2,11 @@ package sharev.member.adapter.outbound.jpa.repository
 
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
-import sharev.account.adapter.outbound.jpa.entity.AccountJpaEntity
 import sharev.member.adapter.outbound.jpa.entity.MemberJpaEntity
 import sharev.member.domain.model.MemberRole
 import sharev.team.adapter.outbound.jpa.entity.TeamJpaEntity
 
 interface MemberRepository : JpaRepository<MemberJpaEntity, Long> {
-    fun findAllByAccount(account: AccountJpaEntity): List<MemberJpaEntity>
-
-    fun findByTeamAndAccount(team: TeamJpaEntity, account: AccountJpaEntity): MemberJpaEntity?
 
     fun findByTeamAndAccountId(team: TeamJpaEntity, accountId: Long): MemberJpaEntity?
 
@@ -18,8 +14,6 @@ interface MemberRepository : JpaRepository<MemberJpaEntity, Long> {
 
     @EntityGraph(attributePaths = ["account"])
     fun findAllByTeam(team: TeamJpaEntity): List<MemberJpaEntity>
-
-    fun findByTeamAndAccountEmail(team: TeamJpaEntity, email: String): MemberJpaEntity?
 
     fun countByTeamAndRole(team: TeamJpaEntity, role: MemberRole): Long
 }
